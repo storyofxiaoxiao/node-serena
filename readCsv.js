21bc5ce475bca0361b83e50f40fb3b61e7a7cad4
serena <2294520219@qq.com>
2021-03-17 15:08:30 +0800

    read file 1.2.1

---
+++ readCsv.js
@@ -0,0 +1,20 @@
+const csvtojson = require("csvtojson");
+const csvFilePath = './upload/excel111.csv';
+
+// ！！！important, only support .csv , can't be .xlsx
+/**
+ *  Task: convert csvfile to jsonobject
+ */
+csvtojson()
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+        console.log('jsonObj: ', jsonObj);
+
+        /**
+         * [
+         * 	{a:"1", b:"2", c:"3"},
+         * 	{a:"4", b:"5". c:"6"}
+         * ]
+         */
+
+    });
