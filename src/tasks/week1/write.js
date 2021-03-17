d6349e12117bbcb2824214327c45d91f8513478e
serena <2294520219@qq.com>
2021-03-17 16:23:11 +0800

    task2

--- src/tasks/week1/write.js
+++ src/tasks/week1/write.js
@@ -1,21 +1,24 @@
 const csvtojson = require("csvtojson");
 const FILE_PATH = require('../../utils/consts')
 const fs = require('fs');
+// fs.readFile()
+// csvtojson({ output: "line" })
+//     .fromFile(FILE_PATH.FILE_CSV)
+//     .subscribe((csvLine) => {
+//         console.log('csvLine111: ', csvLine);
+//         // csvLine =>  "1,2,3" and "4,5,6"
+
+//         fs.appendFile('src/output/write.txt', csvLine, function (err) {
+//             if (err) throw err;
+//             console.log('Saved!');
+//         });
+
+//     })
 /**
  *  Task: convert csvfile to jsonobject line by line
  */
-// fs.readFile()
-csvtojson({ output: "line" })
-    .fromFile(FILE_PATH.FILE_CSV)
-    .subscribe((csvLine) => {
-        console.log('csvLine111: ', csvLine);
-        // csvLine =>  "1,2,3" and "4,5,6"
-
-        // fs.appendFile('writeLine.txt', csvLine, function (err) {
-        //     if (err) throw err;
-        //     console.log('Saved!');
-        // });
-
-    })
-// const writeStream = fs.createWriteStream('/output');
-// writeStream
+const writeStream = fs.createWriteStream('src/output/writeText.txt', { encoding: 'utf8' });
+fs.createReadStream(FILE_PATH.FILE_CSV)
+    .pipe(csvtojson())
+    .on('data', (data) => writeStream.write(data))
+    .on('error', (err) => { console.log('err message======', err) })
