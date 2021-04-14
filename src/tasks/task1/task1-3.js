70b515fbe7cfd68d2a11b3868e66b09e965e642d
serena <2294520219@qq.com>
2021-04-14 15:52:56 +0800

    add babel

--- src/tasks/task1/task1-3.js
+++ src/tasks/task1/task1-3.js
@@ -4,8 +4,11 @@ import fs from 'fs';
 /**
  *  Task: convert csvfile to jsonobject line by line
  */
+function logger(err) {
+    console.log('err message======', err)
+}
 const writeStream = fs.createWriteStream('src/output/writeText.txt', { encoding: 'utf8' });
 fs.createReadStream(FILE_PATH.FILE_CSV)
     .pipe(csv())
     .on('data', (data) => writeStream.write(data))
-    .on('error', (err) => { console.log('err message======', err) })
+    .on('error', (err) => { logger(err) })
